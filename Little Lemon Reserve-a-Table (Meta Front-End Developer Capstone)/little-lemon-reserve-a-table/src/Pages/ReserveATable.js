30f4decac77bb525ae981c1dae0272fe975f3966
Collin Vesel <cvesel217@gmail.com>
2024-10-07 21:34:26 -0500

    Started working on the layout for the first page of the reservation form for the capstone project.

--- Little Lemon Reserve-a-Table (Meta Front-End Developer Capstone)/little-lemon-reserve-a-table/src/Pages/ReserveATable.js
+++ Little Lemon Reserve-a-Table (Meta Front-End Developer Capstone)/little-lemon-reserve-a-table/src/Pages/ReserveATable.js
@@ -1,3 +1,5 @@
+import './ReserveATable.css';
+
 import restaurant from "../Assets/restaurant.jpg";
 
 import ReservationHero from '../Components/ReservationHero'
@@ -6,39 +8,40 @@ export default function ReserveATable() {
     return (<>
         <ReservationHero headerText="Reserve a Table" photo={restaurant}/>
         <main>
-            <form>
-                <label htmlFor="reservationDate">Choose a date:</label>
-                <input type="date" id="reservationDate" name="reservationDate" required/>
-                
-                <label htmlFor="reservationTime">Choose a time:</label>
-                <input type="time" id="reservationTime" name="reservationTime" required/>
-
-                {/* TODO: Figure out how to do the "Number of guests" field.
-                Maybe I can just use a number stepper and do a heck of a lot
-                of CSS to get it to look like I want it to. */}
-                <label htmlFor="numOfGuests">Number of guests:</label>
-                <button type="button">-</button>
-                <span id="numGuests">5</span>
-                <button type="button">+</button>
-
-                <label htmlFor="seatingChoice">Where would you like to sit?</label>
-                <fieldset id="seatingChoice" name="seatingChoice">
-                    <input type="radio" value="Inside" id="InsideRadio"/><label htmlFor="InsideRadio">Inside</label>
-                    <input type="radio" value="Outside" id="OutsideRadio"/><label htmlFor="OutsideRadio">Outside</label>
-                    <input type="radio" value="No Preference" id="NoPreferenceRadio"/><label htmlFor="NoPreferenceRadio">No Preference</label>
-                </fieldset>
-
-                <label htmlFor="occasion">Is it a special occasion?</label>
-                <select id="occasion" name="occasion">
-                    <option value="birthday">Birthday</option>
-                    <option value="engagement">Engagement</option>
-                    <option value="anniversary">Anniversary</option>
-                    <option value="other">Other</option>
-                </select>
-
-                <label htmlFor="comments">Additional comments? (i.e., any special isntructions or accommodations needed):</label>
-                <textarea id="comments" name="comments"/>
-
+            <form className="ReserveForm">
+                <div>
+                    <label htmlFor="reservationDate">Choose a date:</label>
+                    <input type="date" id="reservationDate" name="reservationDate" required/>
+                </div>
+                <div>
+                    <label htmlFor="reservationTime">Choose a time:</label>
+                    <input type="time" id="reservationTime" name="reservationTime" required/>
+                </div>
+                <div>
+                    <label htmlFor="numOfGuests">Number of guests:</label>
+                    <input type="number" id="numGuests" label="numGuests" required min="1"/>
+                </div>
+                <div>
+                    <label htmlFor="seatingChoice">Where would you like to sit?</label>
+                    <fieldset id="seatingChoice" name="seatingChoice">
+                        <input type="radio" value="Inside" id="InsideRadio"/><label htmlFor="InsideRadio">Inside</label>
+                        <input type="radio" value="Outside" id="OutsideRadio"/><label htmlFor="OutsideRadio">Outside</label>
+                        <input type="radio" value="No Preference" id="NoPreferenceRadio"/><label htmlFor="NoPreferenceRadio">No Preference</label>
+                    </fieldset>
+                </div>
+                <div>
+                    <label htmlFor="occasion">Is it a special occasion?</label>
+                    <select id="occasion" name="occasion">
+                        <option value="birthday">Birthday</option>
+                        <option value="engagement">Engagement</option>
+                        <option value="anniversary">Anniversary</option>
+                        <option value="other">Other</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="comments">Additional comments? (i.e., any special isntructions or accommodations needed):</label>
+                    <textarea id="comments" name="comments"/>
+                </div>
                 <button type="submit">Submit Reservation</button>
             </form>
         </main>
