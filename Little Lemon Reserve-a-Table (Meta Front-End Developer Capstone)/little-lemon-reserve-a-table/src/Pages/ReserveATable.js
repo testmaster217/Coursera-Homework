11c7190de14a1b444d48b7e99d00a43d611c236a
Collin Vesel <cvesel217@gmail.com>
2024-10-09 17:04:34 -0500

    Added styling for the various elements on the first page of the reservation form for the capstone project. They don't look quite how I want them to look, but I will see later in the project if I should/was supposed to use a UI framework to make them instead of using native elements that are aparently not as customizable as I want.

--- Little Lemon Reserve-a-Table (Meta Front-End Developer Capstone)/little-lemon-reserve-a-table/src/Pages/ReserveATable.js
+++ Little Lemon Reserve-a-Table (Meta Front-End Developer Capstone)/little-lemon-reserve-a-table/src/Pages/ReserveATable.js
@@ -8,34 +8,37 @@ export default function ReserveATable() {
     return (<>
         <ReservationHero headerText="Reserve a Table" photo={restaurant}/>
         <main>
+            {/* TODO: May need to replace the native form elements with stuff from
+            a UI framework for better customizability. See if it comes up later in
+            the project. */}
             <form className="ReserveForm">
                 {/* TODO: Add error messages that appear when a field is invalid.
                 (Might come up later in the project.) */}
                 <div>
-                    <label htmlFor="reservationDate"><span className='HighlightText'>*</span>Choose a date:</label>
+                    <label htmlFor="reservationDate" className='ParagraphText'><span className='HighlightText'>*</span>Choose a date:</label>
                     {/* TODO: Set today's date as the min and the value. */}
-                    <input type="date" id="reservationDate" name="reservationDate" required/>
+                    <input type="date" id="reservationDate" name="reservationDate" required className='FormField LeadText'/>
                 </div>
                 <div>
-                    <label htmlFor="reservationTime"><span className='HighlightText'>*</span>Choose a time:</label>
+                    <label htmlFor="reservationTime" className='ParagraphText'><span className='HighlightText'>*</span>Choose a time:</label>
                     {/* TODO: Set the current time as the min and the value. */}
-                    <input type="time" id="reservationTime" name="reservationTime" required/>
+                    <input type="time" id="reservationTime" name="reservationTime" required className='FormField LeadText'/>
                 </div>
                 <div>
-                    <label htmlFor="numOfGuests"><span className='HighlightText'>*</span>Number of guests:</label>
-                    <input type="number" id="numGuests" label="numGuests" value="1" required min="1"/>
+                    <label htmlFor="numOfGuests" className='ParagraphText'><span className='HighlightText'>*</span>Number of guests:</label>
+                    <input type="number" id="numOfGuests" name="numOfGuests" value="1" required min="1" className='FormField LeadText'/>
                 </div>
                 <div>
-                    <label htmlFor="seatingChoice">Where would you like to sit?</label>
+                    <label htmlFor="seatingChoice" className='ParagraphText'>Where would you like to sit?</label>
                     <fieldset id="seatingChoice" name="seatingChoice">
-                        <input type="radio" value="Inside" id="InsideRadio"/><label htmlFor="InsideRadio">Inside</label>
-                        <input type="radio" value="Outside" id="OutsideRadio"/><label htmlFor="OutsideRadio">Outside</label>
-                        <input type="radio" value="No Preference" id="NoPreferenceRadio" checked/><label htmlFor="NoPreferenceRadio">No Preference</label>
+                        <input type="radio" value="Inside" id="InsideRadio"/><label htmlFor="InsideRadio" className='ParagraphText'>Inside</label>
+                        <input type="radio" value="Outside" id="OutsideRadio"/><label htmlFor="OutsideRadio" className='ParagraphText'>Outside</label>
+                        <input type="radio" value="No Preference" id="NoPreferenceRadio" checked/><label htmlFor="NoPreferenceRadio" className='ParagraphText'>No Preference</label>
                     </fieldset>
                 </div>
                 <div>
-                    <label htmlFor="occasion">Is it a special occasion?</label>
-                    <select id="occasion" name="occasion" value="nothing">
+                    <label htmlFor="occasion" className='ParagraphText'>Is it a special occasion?</label>
+                    <select id="occasion" name="occasion" value="nothing" className='FormDropDown LeadText'>
                         <option value="nothing">Nothing special</option>
                         <option value="birthday">Birthday</option>
                         <option value="engagement">Engagement</option>
@@ -44,10 +47,10 @@ export default function ReserveATable() {
                     </select>
                 </div>
                 <div>
-                    <label htmlFor="comments">Additional comments? (i.e., any special isntructions or accommodations needed):</label>
+                    <label htmlFor="comments" className='ParagraphText'>Additional comments? (i.e., any special isntructions or accommodations needed):</label>
                     <textarea id="comments" name="comments"/>
                 </div>
-                <button type="submit">Submit Reservation</button>
+                <button type="submit" className='MainButton LeadText'>Submit Reservation</button>
             </form>
         </main>
     </>);
