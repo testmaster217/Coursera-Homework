b027d50181cad19b57932090e18f6805b38186db
Collin Vesel <cvesel217@gmail.com>
2024-10-16 00:28:03 -0500

    Made the hamburger button work. Just need to make it so that clickign the links closes the menu.

--- Little Lemon Reserve-a-Table (Meta Front-End Developer Capstone)/little-lemon-reserve-a-table/src/Components/NavBar.js
+++ Little Lemon Reserve-a-Table (Meta Front-End Developer Capstone)/little-lemon-reserve-a-table/src/Components/NavBar.js
@@ -6,17 +6,20 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBars} from "@fortawesome/free-solid-svg-icons";
 
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function NavBar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return <nav>
         <div className='Icons'>
             <Link to="/" aria-label='Little Lemon homepage'><img src={logo} aria-hidden/></Link>
-            <button type="button">
+            <button type="button" onClick={() => setMenuOpen(!menuOpen)}>
                 <span className='ParagraphText'>MENU</span>
                 <FontAwesomeIcon icon={faBars}/>
             </button>
         </div>
-        <menu>
+        <menu className='DesktopMenu'>
             <li><Link to="/" className='ParagraphText'>HOME</Link></li>
             <li><Link to="/#about-us" className='ParagraphText'>ABOUT</Link></li>
             <li><Link to="/" className='ParagraphText'>MENU</Link></li>
@@ -24,5 +27,13 @@ export default function NavBar() {
             <li><Link to="/" className='ParagraphText'>ORDER ONLINE</Link></li>
             <li><Link to="/" className='ParagraphText'>LOGIN</Link></li>
         </menu>
+        {menuOpen && <menu>
+            <li><Link to="/" className='ParagraphText'>HOME</Link></li>
+            <li><Link to="/#about-us" className='ParagraphText'>ABOUT</Link></li>
+            <li><Link to="/" className='ParagraphText'>MENU</Link></li>
+            <li><Link to="/reserve-a-table" className='ParagraphText'>RESERVATIONS</Link></li>
+            <li><Link to="/" className='ParagraphText'>ORDER ONLINE</Link></li>
+            <li><Link to="/" className='ParagraphText'>LOGIN</Link></li>
+        </menu>}
     </nav>;
 }
