f3ee89c4738619d3d5fef9fc50c1ca14c2b4ed3d
Collin Vesel <cvesel217@gmail.com>
2025-02-05 00:31:13 -0600

    Added filtering, sorting, and pagination for the 'MenuItems' API endpoint. Again, I still need to test it.

--- Django APIs Final Project (Meta APIs Final Project)/Project/LittleLemon/LittleLemonAPI/views.py
+++ Django APIs Final Project (Meta APIs Final Project)/Project/LittleLemon/LittleLemonAPI/views.py
@@ -9,6 +9,8 @@ from .models import MenuItem
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    ordering_fields = ['price']
+    search_fields = ['title', 'category__title']
 
     def get_permissions(self):
         if self.request.method == 'GET':
