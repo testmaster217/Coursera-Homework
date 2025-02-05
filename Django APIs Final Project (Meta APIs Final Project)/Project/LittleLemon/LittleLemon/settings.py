fa17594f00a6a28b945bd0206c1aab2f2bc8fb54
Collin Vesel <cvesel217@gmail.com>
2025-02-05 10:35:29 -0600

    Registered some models with the admin panel so that admins can create and change instances of them, removed a filtering framework that I didn't have nstalled from the settings.py (hopefully I didn't need it), and added a .gitignore that I'm hoping will keep the __pycache__ files out of the repo.

--- Django APIs Final Project (Meta APIs Final Project)/Project/LittleLemon/LittleLemon/settings.py
+++ Django APIs Final Project (Meta APIs Final Project)/Project/LittleLemon/LittleLemon/settings.py
@@ -132,12 +132,11 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_FILTER_BACKENDS": [
-        "django_filters.rest_framework.DjangoFilterBackend",
         'rest_framework.filters.OrderingFilter',
         'rest_framework.filters.SearchFilter',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    "PAGE_SIZE": 5
+    "PAGE_SIZE": 5,
 }
 
 DJOSER = {
