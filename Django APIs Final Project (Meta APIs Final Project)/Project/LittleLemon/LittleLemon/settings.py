4872028f641ae4b40afbdc0c3311ddb96afa82ca
Collin Vesel <cvesel217@gmail.com>
2025-02-04 21:45:20 -0600

    Added Djoser authentication configuration and some endpoints.

--- Django APIs Final Project (Meta APIs Final Project)/Project/LittleLemon/LittleLemon/settings.py
+++ Django APIs Final Project (Meta APIs Final Project)/Project/LittleLemon/LittleLemon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'LittleLemonAPI',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    )
+}
+
+DJOSER = {
+    "USER_ID_FIELD": "username"
+}
