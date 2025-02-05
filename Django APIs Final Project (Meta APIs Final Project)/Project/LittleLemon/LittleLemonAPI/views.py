8e8db31f72853f9a86ecbeb9f4b2b60574f37842
Collin Vesel <cvesel217@gmail.com>
2025-02-05 00:23:40 -0600

    Added MenuItem API endpoints and authorization checks to make sure that only managers can do certain things. Still need to test these, though.

--- Django APIs Final Project (Meta APIs Final Project)/Project/LittleLemon/LittleLemonAPI/views.py
+++ Django APIs Final Project (Meta APIs Final Project)/Project/LittleLemon/LittleLemonAPI/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .serializers import MenuItemSerializer
+from .models import MenuItem
 
 # Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return []
+        return [IsAuthenticated()]
+
+    def post(self, request, *args, **kwargs):
+        if self.request.user.groups.filter(name='Manager').exists():
+            return super().post(request, *args, **kwargs)
+        return Response({"message": "Only managers can do that."}, 403)
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return []
+        return [IsAuthenticated()]
+
+    def put(self, request, *args, **kwargs):
+        if self.request.user.groups.filter(name='Manager').exists():
+            return super().put(request, *args, **kwargs)
+        return Response({"message": "Only managers can do that."}, 403)
+
+    def patch(self, request, *args, **kwargs):
+        if self.request.user.groups.filter(name='Manager').exists():
+            return super().patch(request, *args, **kwargs)
+        return Response({"message": "Only managers can do that."}, 403)
+
+    def delete(self, request, *args, **kwargs):
+        if self.request.user.groups.filter(name='Manager').exists():
+            return super().delete(request, *args, **kwargs)
+        return Response({"message": "Only managers can do that."}, 403)
