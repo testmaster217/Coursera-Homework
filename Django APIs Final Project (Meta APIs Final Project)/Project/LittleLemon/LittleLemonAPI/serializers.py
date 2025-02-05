e44f52a5aec83a48a59ddf745ed6854ccd637fc6
Collin Vesel <cvesel217@gmail.com>
2025-02-05 11:32:31 -0600

    Added a serializer for menu categories and fixed an issue where they were labelled as 'Category Object 1' etc. in the admin panel after creating them.

--- Django APIs Final Project (Meta APIs Final Project)/Project/LittleLemon/LittleLemonAPI/serializers.py
+++ Django APIs Final Project (Meta APIs Final Project)/Project/LittleLemon/LittleLemonAPI/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import MenuItem
+from .models import MenuItem, Category
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = ['id', 'title', 'price', 'featured', 'category']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'slug', 'title']
