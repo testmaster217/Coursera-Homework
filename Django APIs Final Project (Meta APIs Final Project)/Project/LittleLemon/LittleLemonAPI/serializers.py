af68e3adb51bf0b08893769cc43d53638499f288
Collin Vesel <cvesel217@gmail.com>
2025-02-06 14:53:39 -0600

    Added a serializer for the Cart model in the API Final Project.

--- Django APIs Final Project (Meta APIs Final Project)/Project/LittleLemon/LittleLemonAPI/serializers.py
+++ Django APIs Final Project (Meta APIs Final Project)/Project/LittleLemon/LittleLemonAPI/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import MenuItem, Category
+
+from .models import Cart, Category, MenuItem, Order, OrderItem
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,13 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'slug', 'title']
+
+class CartSerializer(serializers.ModelSerializer):
+    price = serializers.SerializerMethodField(method_name='calculate_price')
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'menuitem', 'quantity', 'unit_price', 'price']
+
+    def calculate_price(self, cart:Cart):
+        return cart.quantity * cart.unit_price
